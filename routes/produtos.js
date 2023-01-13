f98c543da999b6f161e90744bfc6ddcb15e9c9c9
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2023-01-13 13:19:47 -0300

    Bug fix

--- routes/produtos.js
+++ routes/produtos.js
@@ -6,3 +6,9 @@ router.get('/', (req, res, next) => {
         mensagem: 'sucesso, produtos'
     });
 });
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        Mensagem: 'usando post dentro da rota de produtos'
+    })
+});
