f22c793217d3ad7378696d2157da391b867f403a
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2023-01-13 08:27:33 -0300

    start

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const rotaProdutos = require('./routes/produtos');
+
+//app.use('/produtos', rotaProdutos);
+app.use('/teste', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'successhhhh'
+    });
+
+});
+
+module.exports = app;
+
