6962c3a0ca7d288f1ddaaf07aa09b3fb1352b6c6
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2023-01-13 13:26:48 -0300

    Another bug fix

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,14 +1,39 @@
-const express = require('express');
+const express = Require('express');
 const router = express.Router();
 
+//RETORNA TODOS OS PRODUTOS
 router.get('/', (req, res, next) => {
     res.status(200).send({
         mensagem: 'sucesso, produtos'
     });
 });
 
+// INSERE UM PRODUTO
 router.post('/', (req, res, next) => {
     res.status(201).send({
         Mensagem: 'usando post dentro da rota de produtos'
     })
 });
+
+// RETORNA UM PRODUTO ESPECIFICO
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+    if  (id === 'especial') {
+        res.status(200).send({
+            mensagem: 'sucesso, id especial',
+            id: id
+        });
+
+    } else {
+        res.status(200).send({
+            mensagem: 'sucesso, produto especifico',
+            id: id
+        });
+
+    }
+});
+
+module.exports = router;
+
+
+
