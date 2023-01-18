a7e86524ee9e5d5a3723335c0a674b37e3f162d1
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2023-01-18 15:59:16 -0300

    nodemon add

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+app.use(morgan('dev'));
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+//qunado nao encontra entra aqui e gera erro
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado')
+    erro.status(404);
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    req.status(error.status || 500);
+    return res.send({
+        erro:{
+            mensagem: error.message
+        }
+    })
+});
+
+
+
 module.exports = app;
+
